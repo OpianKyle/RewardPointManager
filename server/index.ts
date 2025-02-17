e9f1743895dd4889aaaf117aac7dc4fc2e82ac0f
OpianKyle <kylem@opianfsgroup.com>
2025-02-17 13:45:58 +0200

    First commit

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,10 @@
+import 'dotenv/config';
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 import cors from "cors";
 import fileUpload from 'express-fileupload';
+
 
 const app = express();
 
